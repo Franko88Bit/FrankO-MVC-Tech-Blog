d04067f9ab8c940bbd6e2f2cb31d9c320b93c5d3
franko88bit <franks188@icloud.com>
2023-08-12 20:12:02 -0400

    updated web server code to be standard

--- server.js
+++ server.js
@@ -1,56 +1,16 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
-const allRoutes = require("./controllers");
-const session = require("express-session");
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 require('dotenv').config();
-const db = mysql.createConnection(
-    {
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'franko_tech_blog_db',
-        port:8889
-        
-    }
-)
-
-const app = express();
-const PORT = process.env.PORT || 8889;
-
-const { User, Blog, Comment } = require("../models");
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-const sess = {
-    secret: process.env.DB_SESSION_SECRET,
-    cookie: {
-
-        maxAge: 0.5 * 60 * 60 * 1000
-    },
-
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
-
-app.use(session(sess));
-
-app.use(express.static('public'));
-
-const hbs = exphbs.create({});
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use("/", allRoutes);
-
-sequelize.sync({ focus: false }).then(function() {
-    app.listen(PORT, function() {
-        console.log("APP listening on PORT " + PORT);
+        dialect: 'mysql',
+        port: 8889
     });
-});
+}
 
+module.exports = sequelize;
