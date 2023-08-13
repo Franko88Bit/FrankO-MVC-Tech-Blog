d55b3486824a48e65f1530cbf528c93401f45a0a
franko88bit <franks188@icloud.com>
2023-08-12 20:13:35 -0400

    updated server.js and also connection.js

--- server.js
+++ server.js
@@ -1,16 +1,43 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const routes = require('./controllers');
 
-let sequelize;
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 8889
+const sequelize = require('./config/config');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        maxAge: 300000,
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
     });
-}
-
-module.exports = sequelize;
+});
